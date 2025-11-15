cb4713d87127f3a0b98aa6835c8b02a3e9e15bb4
velvet-noir <this.is.romniko@gmail.com>
2025-11-16 00:15:30 +0700

    добавил отчет и код lab_1

--- students/IS22/Nikolaev_Roman/lab_1/main.py
+++ students/IS22/Nikolaev_Roman/lab_1/main.py
@@ -1,17 +1,40 @@
-import sys
-from openai import OpenAI
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
+from openai import OpenAI
+
+from history import clear_history, load_history, save_history
 
 load_dotenv()
 
 api_key = os.getenv("OPENAI_API_KEY")
+system_prompt = os.getenv("SYSTEM_PROMPT")
 
 client = OpenAI(api_key=api_key)
 
+dialog_history = []
+
+MAX_HISTORY = 6
+dialog_history = load_history(MAX_HISTORY)
+
 
 def get_response(text: str, client: OpenAI):
-    response = client.responses.create(model="gpt-4.1-nano", input=text)
+    global dialog_history
+    dialog_history.append({"role": "user", "content": text})
+
+    response = client.responses.create(
+        model="gpt-4.1-nano",
+        input=dialog_history,
+        instructions=system_prompt,
+        temperature=0.8,
+    )
+
+    dialog_history.append({"role": "assistant", "content": response.output_text})
+
+    dialog_history[:] = dialog_history[-MAX_HISTORY:]
+
+    save_history(dialog_history)
+
     return response
 
 
@@ -22,5 +45,11 @@ if __name__ == "__main__":
         if question.lower() == "exit":
             print("Завершение программы.")
             break
+        if question.lower() == "clear":
+            dialog_history = []
+            clear_history()
+            print("История диалога очищена.")
+            continue
+
         answer = get_response(question, client)
         print("AI:", answer.output_text)
